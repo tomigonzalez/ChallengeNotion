85747f059f4bd08a3a4a1fcd275c38fcf6aa4860
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-04-12 21:09:33 -0300

    ASD

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,30 +1,28 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { LucideCoffee } from "../Icons/Coffe";
 import { LucideHome } from "../Icons/Home";
 import NavLinks from "../NavLinks/NavLinks";
 
 const Nav = () => {
-  const navigate = useNavigate();
-  const handleClick = () => navigate("/#");
   return (
     <header className="w-full h-20 bg-sky-600 items-center justify-between flex">
-      <div className="h-full w-1/5 flex justify-around items-center cursor-pointer ">
-        <a onClick={handleClick}>
-          <h1 className="text-black curso hover:text-white">
+      <div className="h-full w-1/5 flex justify-around items-center  ">
+        <Link to={"#"}>
+          <h1 className="text-black cursor-pointer hover:text-white">
             <LucideHome fontSize={45}></LucideHome>
           </h1>
-        </a>
+        </Link>
       </div>
-      <nav className="flex w-2/4 h-full justify-around items-center cursor-pointer sm:w-2/5 ">
+      <nav className="flex w-2/4 h-full justify-around items-center  sm:w-2/5 ">
         <NavLinks link="/reservas">
-          <div className="flex flex-col items-center ">
+          <div className="flex flex-col items-center cursor-pointer ">
             <LucideCoffee fontSize={40}></LucideCoffee>
             <p>Reservar</p>
           </div>
         </NavLinks>
 
         <NavLinks link="/misturnos">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center cursor-pointer">
             <LucideCoffee fontSize={40}></LucideCoffee>
             <p>Mis turnos</p>
           </div>
--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -1,26 +1,25 @@
 import { useLocation } from "react-router";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 type Props = {
   link: string;
   children: string | JSX.Element;
 };
 
 const NavLinks = (props: Props) => {
-  const navigate = useNavigate();
   const location = useLocation();
 
   const estaActivo = location.pathname === props.link;
-  const handleClick = () => navigate(props.link);
+
   return (
-    <a
-      onClick={handleClick}
+    <Link
+      to={props.link}
       className={`${
         estaActivo ? "text-white" : "text-black"
       } h-full pt-3 w-20 flex flex-col items-center  hover:text-white`}
     >
       {props.children}
       {estaActivo && <span className="w-full h-2 bg-white"></span>}
-    </a>
+    </Link>
   );
 };
 
